58e29d512b9fe6576f24fbbab1b17d5fa2d7c364
Raqbit <raqbit@outlook.com>
2018-11-30 16:33:57 +0100

    Add test for IXapi service discovery

--- src/app/core/ix-api/ix-api.service.spec.ts
+++ src/app/core/ix-api/ix-api.service.spec.ts
@@ -1,17 +1,47 @@
-import {inject, TestBed} from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
 
 import {IXApiService} from './ix-api.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {ApiResponse, Link} from './ix-api-responses';
+import {environment} from '../../../environments/environment';
 
 describe('IXApiService', () => {
+  let injector: TestBed;
+  let service: IXApiService;
+  let httpMock: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [IXApiService]
     });
+    injector = getTestBed();
+    service = injector.get(IXApiService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should be created', inject([IXApiService], (service: IXApiService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('Should correctly fetch and transform a discovery response', () => {
+    const dummyDisovery = [
+      new Link('foo.bar', 'foo'),
+      new Link('nu.nl', 'nu'),
+      new Link('bing.com', 'bing'),
+      new Link('google.com', 'google')
+    ];
+    const mockResponse = <ApiResponse<void>>{
+      links: dummyDisovery
+    };
+    service.discoverApiEndpoints().subscribe(() => {
+      expect(Object.values(service.apiEndpoints).length).toBe(4);
+      for (const link of dummyDisovery) {
+        expect(service.apiEndpoints[link.rel]).toBe(link.href);
+      }
+    });
+
+    const req = httpMock.expectOne(`${environment.ixApiDiscoveryUrl}/`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
 });
